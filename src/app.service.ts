26d3fd27d515d2434b2c2abec16b5a436074ff16
corocn <crsk.plmzh@gmail.com>
2021-08-22 22:18:01 +0900

    ogp

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
+import * as fs from 'fs';
+import handlebars from 'handlebars';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 630 });
+    const hbsFile = fs.readFileSync('src/templates/default.html.hbs', 'utf8');
+
+    const template = handlebars.compile(hbsFile);
+    const html = template({ title: 'これはタイトルですこれはタイトルですこれはタイトルです' });
+
+    page.setContent(html);
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.evaluateHandle('document.fonts.ready');
+    await page.screenshot({ path: './tmp/example.png' });
+
+    return 'OK';
   }
 }
